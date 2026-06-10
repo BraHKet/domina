597f7d3bd35234d3e824c4ad29d35488e50703b8
Lorenzo R <lore.mail.gl@gmail.com>
2026-06-10 06:47:35 +0200

    fix: add @vercel/analytics dependency and initialize analytics in main component

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import 'leaflet/dist/leaflet.css'
 import './index.css'
 import App from './App.jsx'
+import { inject } from '@vercel/analytics'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+inject()
